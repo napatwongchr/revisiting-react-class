fa96aebacc2381206c28dde467a0cda99e126c8d
napatwongchr <napat.wongchr@gmail.com>
2023-08-27 13:33:07 +0700

    feat: complete mission 4

--- client/src/components/ProductHighlight.jsx
+++ client/src/components/ProductHighlight.jsx
@@ -1,26 +1,36 @@
 import ProductCard from "./ProductCard";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function ProductHighlight() {
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    const productDataFromServer = await axios.get(
+      "http://localhost:4000/products"
+    );
+    setProducts(productDataFromServer.data);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
     <section className="bg-gray-200 py-8">
       <div className="container mx-auto">
         <h2 className="text-2xl font-semibold mb-4">Featured Products</h2>
         <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-          <ProductCard
-            imgSrc="https://picsum.photos/id/231/200/200"
-            productName="The Lorem Ipsum for photos."
-            productPrice={99.99}
-          />
-          <ProductCard
-            imgSrc="https://picsum.photos/id/232/200/200"
-            productName="Wireless Bluetooth Earbuds"
-            productPrice={79.99}
-          />
-          <ProductCard
-            imgSrc="https://picsum.photos/200/200"
-            productName="Stylish Cool Sunglasses in The World"
-            productPrice={79.99}
-          />
+          {products.map((item) => {
+            return (
+              <ProductCard
+                key={item.id}
+                imgSrc={item.image}
+                productName={item.name}
+                productPrice={item.price}
+              />
+            );
+          })}
         </div>
       </div>
     </section>
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { products } from "./data/products.js";
 import cors from "cors";
 
